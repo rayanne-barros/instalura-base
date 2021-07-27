431fb5be185c3ac85155202cb0551bf12dc79663
Rayanne Barros <rayanneb126@gmail.com>
2021-07-27 19:24:08 -0300

    Aula 2

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
-import styled from 'styled-components'
+import Menu from '../src/components/commons/Menu'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <div>
+      <Menu/>     
+    </div>
+   
+  )
+
+
 }
