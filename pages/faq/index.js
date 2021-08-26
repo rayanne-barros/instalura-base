6e04298cd0eee60bd9fb5a0a745a22b05abdef87
Rayanne Barros <rayanneb126@gmail.com>
2021-08-26 00:46:30 -0300

    feat: adicionando faq dinâmico

--- pages/faq/index.js
+++ pages/faq/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import FAQScreen from '../../src/components/screens/FAQScreen';
 
-export default function FAQPage() {
+// eslint-disable-next-line react/prop-types
+export default function FAQPage({ faqCategories }) {
+  // const [faqCategories, setFaqCategories] = React.useState([]);
+  // React.useEffect(() => {
+  //   fetch('https://instalura-api.vercel.app/api/content/faq')
+  //     .then((respostaDoServer) => respostaDoServer.json())
+  //     .then((respostaConvertida) => respostaConvertida.data)
+  //     .then((resposta) => {
+  //       setFaqCategories(resposta);
+  //     });
+  // }, []);
   return (
-    <div>
-      Página do FAQ
-    </div>
+    <FAQScreen faqCategories={faqCategories} />
   );
+}
+export async function getStaticProps() {
+  const faqCategories = await fetch('https://instalura-api.vercel.app/api/content/faq')
+    .then((respostaDoServer) => respostaDoServer.json())
+    .then((respostaConvertida) => respostaConvertida.data);
+  return {
+    props: {
+      faqCategories,
+    }, // will be passed to the page component as props
+  };
 }
