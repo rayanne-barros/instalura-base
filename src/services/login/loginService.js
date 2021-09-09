04c6f98bba76c9a9365e1ac2d4ca99fb20e16e3f
Rayanne Barros <rayanneb126@gmail.com>
2021-09-09 00:30:07 -0300

    Criando: isStagingEnv e  definindo BASE_URL

--- src/services/login/loginService.js
+++ src/services/login/loginService.js
@@ -1,4 +1,5 @@
 import { setCookie, destroyCookie } from 'nookies';
+import { isStagingEnv } from '../../infra/env/isStagingEnv';
 
 async function HttpClient(url, { headers, body, ...options }) {
   return fetch(url, {
@@ -18,9 +19,13 @@ async function HttpClient(url, { headers, body, ...options }) {
     });
 }
 
+const BASE_URL = isStagingEnv
+  ? 'https://instalura-api-git-master-omariosouto.vercel.app'
+  : 'https://instalura-api.omariosouto.vercel.app';
+
 export const loginService = {
   async login({ username, password }) {
-    return HttpClient('https://instalura-api-git-master-omariosouto.vercel.app/api/login', {
+    return HttpClient(`${BASE_URL}/api/login`, {
       method: 'POST',
       body: {
         username,
