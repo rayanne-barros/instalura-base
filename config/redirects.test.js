371cbfaacb2ae8742fb904bff719a16615bdd218
Rayanne Barros <rayanneb126@gmail.com>
2021-09-09 21:25:50 -0300

    Refatorando o teste de redirects usando snapshot

--- config/redirects.test.js
+++ config/redirects.test.js
@@ -3,13 +3,6 @@ import redirects from './redirects';
 // Test Drive Development
 describe('config/redirects', () => {
   test('renders all current redirects', () => {
-    const redirectsAtuais = [
-      {
-        source: '/login/',
-        destination: '/app/login/',
-        permanent: true,
-      },
-    ];
-    expect(redirects).toEqual(redirectsAtuais);
+    expect(redirects).toMatchSnapshot();
   });
 });
