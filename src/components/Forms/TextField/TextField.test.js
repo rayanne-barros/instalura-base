eb58e8fdad39ed9af97590b1592db52f087da535
Rayanne Barros <rayanneb126@gmail.com>
2021-09-10 01:55:05 -0300

    testUtils usado no import de render e screen

--- src/components/Forms/TextField/TextField.test.js
+++ src/components/Forms/TextField/TextField.test.js
@@ -1,19 +1,15 @@
-import { render, screen } from '@testing-library/react';
 import React from 'react';
+import { render, screen } from '../../../infra/test/testUtils';
 
 import TextField from './index';
-import WebsiteGlobalProvider from '../../wrappers/WebsitePage/provider';
 
 describe('<TextField />', () => {
   test('renders component', () => {
     render(
-      <WebsiteGlobalProvider>
-        <TextField placeholder="Nome" value="ju" onChange={() => {}} name="nome" />
-      </WebsiteGlobalProvider>,
+      <TextField placeholder="Nome" value="ju" onChange={() => {}} name="nome" />,
     );
 
     const textField = screen.getByPlaceholderText(/nome/i);
-    console.log(textField);
     expect(textField).toMatchSnapshot();
   });
 });
