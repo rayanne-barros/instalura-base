9d047ffcd02b25b9c26929754f231d8d6d1ef389
Rayanne Barros <rayanneb126@gmail.com>
2021-09-24 00:30:21 -0300

    Segurança do token

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,36 +1,6 @@
 import React from 'react';
-import { GraphQLClient, gql } from 'graphql-request';
-import AboutScreen from '../src/components/screens/AboutScreen';
+import AboutScreen, { getContent } from '../src/components/screens/AboutScreen';
 import websitePageHOC from '../src/components/wrappers/WebsitePage/hoc';
-
-export async function getStaticProps() {
-  const TOKEN = '451cddafcaad003759a3da414b8283';
-
-  const DatoCMSURL = 'https://graphql.datocms.com/';
-
-  const client = new GraphQLClient(DatoCMSURL, {
-    headers: {
-      Authorization: `Bearer ${TOKEN}`,
-    },
-  });
-
-  const query = gql`
-    query {
-      pageSobre {
-        pageTitle
-        pageDescription
-      }
-    }
-  `;
-
-  const messages = await client.request(query);
-
-  return {
-    props: {
-      messages,
-    },
-  };
-}
 
 function AboutPage({ messages }) {
   return (
@@ -47,3 +17,13 @@ export default websitePageHOC(AboutPage, {
     },
   },
 });
+
+export async function getStaticProps() {
+  const messages = await getContent();
+
+  return {
+    props: {
+      messages,
+    },
+  };
+}
