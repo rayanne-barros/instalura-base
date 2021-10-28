49e085ed2df7f96e1c07e15cdf3464a9b5623639
Rayanne Barros <rayanneb126@gmail.com>
2021-10-28 02:46:02 -0300

    Alterando o menu logado

--- src/components/commons/LoggedMenu/style/MenuWraper.js
+++ src/components/commons/LoggedMenu/style/MenuWraper.js
@@ -6,35 +6,36 @@ export const MenuWrapper = styled.nav`
   display: flex;
   align-items: center;
   justify-content: center;
-  /* flex-wrap: wrap; */
-  margin-top: 18px;
-  padding-left: 28px;
-  padding-right: 28px;
+  padding: 18px 28px;
+  z-index: 10;
+  position:fixed;
+  top:0;
+  left:0;
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.background.light.color};
   ${breakpointsMedia({
     md: css`
       justify-content: space-between;
-      margin-top: 32px;
+      /* margin-top: 32px; */
       margin-left: auto;
       margin-right: auto;
-      width: 100%;
-      padding: 0 16px;
-      /* max-width: 768px; */
-    `,
-    lg: css`
-      /* max-width: 1160px;  */
-    `,
-    xl: css`
-      /* max-width: 1222px; */
+      padding: 18px 28px;
     `,
   })}
 `;
 
 MenuWrapper.Logo = styled.div`
-  padding: 0;
-  margin: 0;
+  left: 50%;
+  margin: 0 0 0 -50px;
   order: 1;
+  padding: 0;
+  position:relative;
+  width:100px;
   ${breakpointsMedia({
     md: css`
+        position:initial;
+        margin:0;
+        left:0;
         width: 131px;
         height: 32px;
       `,
@@ -59,19 +60,22 @@ MenuWrapper.Menu = styled.div`
   width: 100%;
   position: fixed;
   bottom:0;
-  background-color: ${({ theme }) => theme.colors.background.light.color};
+  left:0;
+ // background-color: ${({ theme }) => theme.colors.background.light.color};
   border-radius: 24px 24px 0 0;
+
   .searchDiv{
     display:none;
   }
   .searchInput{
-    background: url('/images/search.png') 10px 12px / 10% no-repeat;
+    background: url('/Images/search.png') 5px 7px / 6% no-repeat;
     background-color: ${({ theme }) => theme.colors.background.light.color};
     padding: 12px 20%;
+    width: 250px;
   }
   .searchInput:focus{
     background: none;
-    padding: 12px 16px;
+    padding: 7px 16px;
     transition: ${({ theme }) => theme.transition};
   }
   .addPicButton{
@@ -79,16 +83,34 @@ MenuWrapper.Menu = styled.div`
     transform: scale(2);
   }
   a, button{
-    width:20px;
+    width:30px;
     padding:0;
   }
   img{
-    width:100%;
+    width:30px;
   }
   .userPic{
     border-radius: 50%;
   }
   
+  .header__userButton{
+    position:relative;
+    /* display:inline-block; */
+  }
+  .header__userButton_modal{
+    display: none;
+    flex-direction:column;
+    align-items: flex-start;
+    justify-content: flex-start;
+    position: absolute;
+    box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+    transition: ${({ theme }) => theme.transition};
+    background-color: ${({ theme }) => theme.colors.background.main.color};
+  }
+  .header__userButton:hover .header__userButton_modal{
+    display:flex;
+  }
+
   ${breakpointsMedia({
     md: css`
       width:50%;
@@ -97,19 +119,22 @@ MenuWrapper.Menu = styled.div`
       padding-bottom: 0;
       position:static;
       background-color: transparent;
-    .searchDiv{
-      display:flex;
-  }
-  .addPicButton{
-    order:initial;
-    transform: none;
-  }
-  .searchIcon{
-    display:none;
-  }
-  a, button{
-    width:35px;
-  }
+      justify-content: space-between;
+
+      .searchDiv{
+        display:block;
+        width: auto;
+      }
+      .addPicButton{
+        order:initial;
+        transform: none;
+      }
+      .searchIcon{
+        display:none;
+      }
+      a, button{
+        width:35px;
+      }
     `,
   })}
 `;
