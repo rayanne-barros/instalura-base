8570c45f140c82fb8a7ffaca328abfa3bd1428ce
Rayanne Barros <rayanneb126@gmail.com>
2021-11-01 22:49:20 -0300

    FormPost para add foto

---
+++ src/components/patterns/FormPost/filtersList.js
@@ -0,0 +1,128 @@
+export const filtersList = [
+  {
+    title: 'normal',
+  },
+  {
+    title: '1977',
+  },
+  {
+    title: 'aden',
+  },
+  {
+    title: 'amaro',
+  },
+  {
+    title: 'ashby',
+  },
+  {
+    title: 'brannan',
+  },
+  {
+    title: 'brooklyn',
+  },
+  {
+    title: 'charmes',
+  },
+  {
+    title: 'clarendon',
+  },
+  {
+    title: 'crema',
+  },
+  {
+    title: 'dogpatch',
+  },
+  {
+    title: 'earlybird',
+  },
+  {
+    title: 'gingham',
+  },
+  {
+    title: 'ginza',
+  },
+  {
+    title: 'hefe',
+  },
+  {
+    title: 'helena',
+  },
+  {
+    title: 'hudson',
+  },
+  {
+    title: 'inkwell',
+  },
+  {
+    title: 'juno',
+  },
+  {
+    title: 'kelvin',
+  },
+  {
+    title: 'lark',
+  },
+  {
+    title: 'lofi',
+  },
+  {
+    title: 'ludwig',
+  },
+  {
+    title: 'maven',
+  },
+  {
+    title: 'mayfair',
+  },
+  {
+    title: 'moon',
+  },
+  {
+    title: 'nashville',
+  },
+  {
+    title: 'perpetua',
+  },
+  {
+    title: 'poprocket',
+  },
+  {
+    title: 'reyes',
+  },
+  {
+    title: 'rise',
+  },
+  {
+    title: 'sierra',
+  },
+  {
+    title: 'skyline',
+  },
+  {
+    title: 'slumber',
+  },
+  {
+    title: 'stinson',
+  },
+  {
+    title: 'sutro',
+  },
+  {
+    title: 'toaster',
+  },
+  {
+    title: 'valencia',
+  },
+  {
+    title: 'vesper',
+  },
+  {
+    title: 'walden',
+  },
+  {
+    title: 'willow',
+  },
+  {
+    title: 'xpro-ii',
+  },
+];
